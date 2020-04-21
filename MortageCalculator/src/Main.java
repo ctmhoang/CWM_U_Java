84d7a4384745b3f281af504e6a0bd586e264022a
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2020-04-21 05:45:13 -0400

    Update mortage project with basic errors handling (Lamda expression :smiley:)

--- MortageCalculator/src/Main.java
+++ MortageCalculator/src/Main.java
@@ -1,5 +1,6 @@
 import java.text.NumberFormat;
 import java.util.Scanner;
+import java.util.function.BiFunction;
 
 public class Main {
   public static void main(String[] args) {
@@ -8,22 +9,48 @@ public class Main {
     final byte PERCENTAGE_POINTS = 100;
 
     Scanner input = new Scanner(System.in);
+    NumberFormat format = NumberFormat.getNumberInstance();
 
+    BiFunction<Double, Double, Double> getInputInRange =
+        (low, high) -> {
+          double temp = input.nextDouble();
+          while (temp < low || temp > high) {
+            System.out.println(
+                "Enter a value between "
+                    + format.format(low)
+                    + " and "
+                    + format.format(high)
+                    + " .");
+            temp = input.nextDouble();
+          }
+          return temp;
+        };
+
+    BiFunction<Double, Double, Double> getInputInDomain =
+        (low, high) -> {
+          double temp = input.nextDouble();
+          while (temp <= low || temp >= high) {
+            System.out.println(
+                "Enter a value greater than "
+                    + format.format(low)
+                    + " and less than or equal to "
+                    + format.format(high)
+                    + " .");
+            temp = input.nextDouble();
+          }
+          return temp;
+        };
     System.out.print("Principal($1K - $1M): ");
-    double totalAmount = () -> {
-      double temp = input.nextDouble();
-      while (temp < 1_000 && temp > 1_000_000)
-    {
-      temp = input.nextDouble();
-    }
-      return temp;
-    };
+    double totalAmount = getInputInRange.apply(1_000D, 1_000_000D);
 
     System.out.print("Annual Interest Rate: ");
-    double monthlyRate = input.nextDouble() / MONTHS_PER_YEAR / PERCENTAGE_POINTS;
+    double annualRate = getInputInDomain.apply(0D, 30D);
+    double monthlyRate = annualRate / MONTHS_PER_YEAR / PERCENTAGE_POINTS;
 
     System.out.print("Period (Year): ");
-    short timesToPay = (short) (input.nextShort() * MONTHS_PER_YEAR);
+    short periods = getInputInDomain.apply(0D, 30D).shortValue();
+    short timesToPay = (short) (periods * MONTHS_PER_YEAR);
+    System.out.println("Enter a value between 1 and 30.");
 
     double monthlyPaid =
         totalAmount

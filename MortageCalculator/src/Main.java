4dd2128aa446bd0f2e90e901eb70f8a4ee5bb627
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2020-04-28 11:09:12 -0400

    Clean my whole mess

--- MortageCalculator/src/Main.java
+++ MortageCalculator/src/Main.java
@@ -5,58 +5,43 @@ import java.util.function.BiFunction;
 public class Main {
   public static void main(String[] args) {
     // Avoid mystery number
+    double totalAmount = getNumInput("Principal($1K - $1M)", 1_000D, 1_000_000D);
+
+    double annualRate = getNumInput("Annual Interest Rate", 1, 30);
+
+    byte periods = (byte) getNumInput("Period (Year)", 1, 30);
+
+    double monthlyPaid = calMortgage(totalAmount, annualRate, periods);
+
+    String res = NumberFormat.getCurrencyInstance().format(monthlyPaid);
+    System.out.println("Montage: " + res);
+  }
+
+  public static double getNumInput(String prompt, double low, double high) {
+    Scanner input = new Scanner(System.in);
+    NumberFormat format = NumberFormat.getNumberInstance();
+    double val;
+
+    while (true) {
+      System.out.print(prompt + ": ");
+      val = input.nextDouble();
+      if (val >= low && val <= high) break;
+      System.out.printf(
+          "Enter a value between %s and %s .\n", format.format(low), format.format(high));
+    }
+    return val;
+  }
+
+  public static double calMortgage(double principle, double annualInterest, byte years) {
+
     final byte MONTHS_PER_YEAR = 12;
     final byte PERCENTAGE_POINTS = 100;
 
-    Scanner input = new Scanner(System.in);
-    NumberFormat format = NumberFormat.getNumberInstance();
+    double monthlyRate = annualInterest / MONTHS_PER_YEAR / PERCENTAGE_POINTS;
+    short timesToPay = (short) (years * MONTHS_PER_YEAR);
 
-    BiFunction<Double, Double, Double> getInputInRange =
-        (low, high) -> {
-          double temp = input.nextDouble();
-          while (temp < low || temp > high) {
-            System.out.println(
-                "Enter a value between "
-                    + format.format(low)
-                    + " and "
-                    + format.format(high)
-                    + " .");
-            temp = input.nextDouble();
-          }
-          return temp;
-        };
-
-    BiFunction<Double, Double, Double> getInputInDomain =
-        (low, high) -> {
-          double temp = input.nextDouble();
-          while (temp <= low || temp >= high) {
-            System.out.println(
-                "Enter a value greater than "
-                    + format.format(low)
-                    + " and less than or equal to "
-                    + format.format(high)
-                    + " .");
-            temp = input.nextDouble();
-          }
-          return temp;
-        };
-    System.out.print("Principal($1K - $1M): ");
-    double totalAmount = getInputInRange.apply(1_000D, 1_000_000D);
-
-    System.out.print("Annual Interest Rate: ");
-    double annualRate = getInputInDomain.apply(0D, 30D);
-    double monthlyRate = annualRate / MONTHS_PER_YEAR / PERCENTAGE_POINTS;
-
-    System.out.print("Period (Year): ");
-    short periods = getInputInDomain.apply(0D, 30D).shortValue();
-    short timesToPay = (short) (periods * MONTHS_PER_YEAR);
-    System.out.println("Enter a value between 1 and 30.");
-
-    double monthlyPaid =
-        totalAmount
-            * (monthlyRate * Math.pow(1 + monthlyRate, timesToPay))
-            / (Math.pow(1 + monthlyRate, timesToPay) - 1);
-    String res = NumberFormat.getCurrencyInstance().format(monthlyPaid);
-    System.out.println("Montage: " + res);
+    return principle
+        * (monthlyRate * Math.pow(1 + monthlyRate, timesToPay))
+        / (Math.pow(1 + monthlyRate, timesToPay) - 1);
   }
 }
